8bcfec645be736065eecfc13dfb594fe748a5f99
yamato-code <118909709+jmn04@users.noreply.github.com>
2024-04-29 11:05:16 +0900

    キャッシュ削除ほか

--- rdb/postgresql/script/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-psql -U rdb -h rdb -p 5432 -d rdb -f /script/sql/trancate_all.sql
--- rdb/postgresql/script/sql/trancate_all.sql
+++
@@ -1,8 +0,0 @@
--- 全テーブルのレコードを削除する
-
-TRUNCATE TABLE
-    -- テーブル名
-    -- table_name1,
-    -- table_name2,
-    -- table_name3
-RESTART IDENTITY;
